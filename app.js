ccebed9e508f49256c7eb2abd9d8ecd314ff860a
Luimavar <luimavargasmoreno@gmail.com>
2025-03-08 19:45:04 -0500

    Agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//array para almacenar nombres de amigos ingresados
+let nombreAmigo = [];
+
+//funcion para agregar amigos
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+if (nombre === ""){
+    alert("Ingresa un nombre por favor");
+    return;
+}
+
+nombreAmigo.push(nombre);
+input.value = "";
+}
+
